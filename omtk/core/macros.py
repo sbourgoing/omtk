708886d1c4b92cd764af8aa8bb5af6fa65a3d209
Sebastien Bourgoing <seb.bourgoing@gmail.com>
2019-11-09 15:58:28 -0500

    Remove bad file structure from last merge

--- omtk/core/macros.py
+++
@@ -1,8 +0,0 @@
-"""
-A macro is a simple action that can be in a shelf or in a keyboard shortcut. Nothing fancy.
-"""
-
-
-class BaseMacro(object):
-    def run(self):
-        raise NotImplementedError
--- omtk/macros/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This module contain various tools that can be used to facilitate the rigging workflow with omtk.
-Previously they were in omtk.core.api, however the increasing number of macros necessitate a dedicated directory.
-"""
-
